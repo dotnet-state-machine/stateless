761c2909c9149303eaa1db92e71af6a5aec67b48
Nicholas Blumhardt <nicholas.blumhardt@gmail.com>
2009-08-23 18:31:05 -0700

    Added unit-level tests for transitioning and ignored trigger behaviours (these were previously only tested at the integration level through StateMachine<>)

--- Stateless.Tests/IgnoredTriggerBehaviourFixture.cs
+++ Stateless.Tests/IgnoredTriggerBehaviourFixture.cs
@@ -16,5 +16,32 @@ namespace Stateless.Tests
             State destination = State.A;
             Assert.IsFalse(ignored.ResultsInTransitionFrom(State.B, out destination));
         }
+
+        [Test]
+        public void ExposesCorrectUnderlyingTrigger()
+        {
+            var ignored = new StateMachine<State, Trigger>.IgnoredTriggerBehaviour(
+                Trigger.X, () => true);
+
+            Assert.AreEqual(Trigger.X, ignored.Trigger);
+        }
+
+        [Test]
+        public void WhenGuardConditionFalse_IsGuardConditionMetIsFalse()
+        {
+            var ignored = new StateMachine<State, Trigger>.IgnoredTriggerBehaviour(
+                Trigger.X, () => false);
+
+            Assert.IsFalse(ignored.IsGuardConditionMet);
+        }
+
+        [Test]
+        public void WhenGuardConditionTrue_IsGuardConditionMetIsTrue()
+        {
+            var ignored = new StateMachine<State, Trigger>.IgnoredTriggerBehaviour(
+                Trigger.X, () => true);
+
+            Assert.IsTrue(ignored.IsGuardConditionMet);
+        }
     }
 }
--- Stateless.Tests/TransitioningTriggerBehaviourFixture.cs
+++ Stateless.Tests/TransitioningTriggerBehaviourFixture.cs
@@ -17,5 +17,32 @@ namespace Stateless.Tests
             Assert.IsTrue(transtioning.ResultsInTransitionFrom(State.B, out destination));
             Assert.AreEqual(State.C, destination);
         }
+
+        [Test]
+        public void ExposesCorrectUnderlyingTrigger()
+        {
+            var transtioning = new StateMachine<State, Trigger>.TransitioningTriggerBehaviour(
+                Trigger.X, State.C, () => true);
+
+            Assert.AreEqual(Trigger.X, transtioning.Trigger);
+        }
+
+        [Test]
+        public void WhenGuardConditionFalse_IsGuardConditionMetIsFalse()
+        {
+            var transtioning = new StateMachine<State, Trigger>.TransitioningTriggerBehaviour(
+                Trigger.X, State.C, () => false);
+
+            Assert.IsFalse(transtioning.IsGuardConditionMet);
+        }
+
+        [Test]
+        public void WhenGuardConditionTrue_IsGuardConditionMetIsTrue()
+        {
+            var transtioning = new StateMachine<State, Trigger>.TransitioningTriggerBehaviour(
+                Trigger.X, State.C, () => true);
+
+            Assert.IsTrue(transtioning.IsGuardConditionMet);
+        }
     }
 }
