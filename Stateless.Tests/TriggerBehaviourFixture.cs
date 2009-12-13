a39203a794eb9583fdad652b62d25f75f0a846d7
Nicholas Blumhardt <nicholas.blumhardt@gmail.com>
2009-12-13 15:15:38 +1000

    Remainder of files for dynamic trigger behaviour.

---
+++ Stateless.Tests/TriggerBehaviourFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Stateless.Tests
+{
+    [TestFixture]
+    public class TriggerBehaviourFixture
+    {
+        [Test]
+        public void ExposesCorrectUnderlyingTrigger()
+        {
+            var transtioning = new StateMachine<State, Trigger>.TransitioningTriggerBehaviour(
+                Trigger.X, State.C, () => true);
+
+            Assert.AreEqual(Trigger.X, transtioning.Trigger);
+        }
+
+        [Test]
+        public void WhenGuardConditionFalse_IsGuardConditionMetIsFalse()
+        {
+            var transtioning = new StateMachine<State, Trigger>.TransitioningTriggerBehaviour(
+                Trigger.X, State.C, () => false);
+
+            Assert.IsFalse(transtioning.IsGuardConditionMet);
+        }
+
+        [Test]
+        public void WhenGuardConditionTrue_IsGuardConditionMetIsTrue()
+        {
+            var transtioning = new StateMachine<State, Trigger>.TransitioningTriggerBehaviour(
+                Trigger.X, State.C, () => true);
+
+            Assert.IsTrue(transtioning.IsGuardConditionMet);
+        }
+    }
+}
---
+++ Stateless/DynamicTriggerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stateless
+{
+    public partial class StateMachine<TState, TTrigger>
+    {
+        internal class DynamicTriggerBehaviour : TriggerBehaviour
+        {
+            readonly Func<object[], TState> _destination;
+
+            public DynamicTriggerBehaviour(TTrigger trigger, Func<object[], TState> destination, Func<bool> guard)
+                : base(trigger, guard)
+            {
+                _destination = Enforce.ArgumentNotNull(destination, "destination");
+            }
+
+            public override bool ResultsInTransitionFrom(TState source, object[] args, out TState destination)
+            {
+                destination = _destination(args);
+                return true;
+            }
+        }
+    }
+}
