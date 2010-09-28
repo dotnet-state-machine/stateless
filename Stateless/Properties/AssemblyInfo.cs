9072e06dc71ac9c2c14d8aef30cc590276a80da1
nicholas.blumhardt <nicholas.blumhardt@localhost>
2010-09-28 17:58:25 +1000

    Updated version number to 2.3

--- Stateless/Properties/AssemblyInfo.cs
+++ Stateless/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.1.1")]
-[assembly: AssemblyFileVersion("2.2.1.1")]
+[assembly: AssemblyVersion("2.3.1.1")]
+[assembly: AssemblyFileVersion("2.3.1.1")]
 
 [assembly: InternalsVisibleTo("Stateless.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010077d5f127d9877b3b813017ddefb168b265f8041ec908984bf36071156ff911a7a27b5243decac72586ecf45edfb93c50fdf063639e39edff2238a2b999caf0b2c9767a1517cb1df747b58853503e03e1fb12f1336c35aa97ad8a8bc2984b7cae9f49cad2eedebb8cf59b3bd5f24fd9f3a53bd1a2d8499f4fe0d938873b0101ca")]
 
