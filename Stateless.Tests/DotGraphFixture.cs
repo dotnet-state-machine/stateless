a781de67853b4dce5aba25fbe64c9f994efd6570
Thomas L. Kjeldsen <tlk@closureconsulting.com>
2015-11-02 22:26:50 +0100

    Generate pseudo-states for dynamic triggers

--- Stateless.Tests/DotGraphFixture.cs
+++ Stateless.Tests/DotGraphFixture.cs
@@ -6,7 +6,8 @@ namespace Stateless.Tests
     [TestFixture]
     public class DotGraphFixture
     {
-        bool IsTrue() {
+        bool IsTrue() 
+        {
             return true;
         }
 
@@ -70,6 +71,37 @@ namespace Stateless.Tests
 
             sm.Configure(State.A)
                 .PermitIf(Trigger.X, State.B, IsTrue);
+
+            Assert.AreEqual(expected, sm.ToDotGraph());
+        }
+
+        [Test]
+        public void DestinationStateIsDynamic()
+        {
+            var expected = "digraph {" + System.Environment.NewLine
+                         + " { node [label=\"?\"] unknownDestination_0 };" + System.Environment.NewLine
+                         + " A -> unknownDestination_0 [label=\"X\"];" + System.Environment.NewLine
+                         + "}";
+
+            var sm = new StateMachine<State, Trigger>(State.A);
+            sm.Configure(State.A)
+                .PermitDynamic(Trigger.X, () => State.B);
+
+            Assert.AreEqual(expected, sm.ToDotGraph());
+        }
+
+        [Test]
+        public void DestinationStateIsCalculatedBasedOnTriggerParameters()
+        {
+            var expected = "digraph {" + System.Environment.NewLine
+                         + " { node [label=\"?\"] unknownDestination_0 };" + System.Environment.NewLine
+                         + " A -> unknownDestination_0 [label=\"X\"];" + System.Environment.NewLine
+                         + "}";
+
+            var sm = new StateMachine<State, Trigger>(State.A);
+            var trigger = sm.SetTriggerParameters<int>(Trigger.X);
+            sm.Configure(State.A)
+                .PermitDynamic(trigger, i => i == 1 ? State.B : State.C);
 
             Assert.AreEqual(expected, sm.ToDotGraph());
         }
--- Stateless/DotGraph.cs
+++ Stateless/DotGraph.cs
@@ -8,26 +8,48 @@ namespace Stateless
         /// <summary>
         /// A string representation of the state machine in the DOT graph language.
         /// </summary>
-        /// <returns>A description of all simple source states, triggers and destination states except those defined with PermitDynamic.</returns>
+        /// <returns>A description of all simple source states, triggers and destination states.</returns>
         public string ToDotGraph()
         {
-            List<string> transitions = new List<string>();
+            List<string> lines = new List<string>();
+            List<string> unknownDestinations = new List<string>();
 
-            foreach (var stateCfg in _stateConfiguration) {
+            foreach (var stateCfg in _stateConfiguration) 
+            {
                 TState source = stateCfg.Key;
-                foreach (var behaviours in stateCfg.Value.TriggerBehaviours) {
-                    foreach (TransitioningTriggerBehaviour behaviour in behaviours.Value.Where(b => b is TransitioningTriggerBehaviour)) {
-                        string line = (behaviour.Guard.Method.DeclaringType.Namespace.Equals("Stateless")) ?
-                            string.Format(" {0} -> {1} [label=\"{2}\"];", source, behaviour.Destination, behaviour.Trigger) :
-                            string.Format(" {0} -> {1} [label=\"{2} [{3}]\"];", source, behaviour.Destination, behaviour.Trigger, behaviour.Guard.Method.Name);
+                foreach (var behaviours in stateCfg.Value.TriggerBehaviours) 
+                {
+                    foreach (TriggerBehaviour behaviour in behaviours.Value) 
+                    {
+                        string destination;
 
-                        transitions.Add(line);
+                        if (behaviour is TransitioningTriggerBehaviour)
+                        {
+                            destination = ((TransitioningTriggerBehaviour)behaviour).Destination.ToString ();
+                        } 
+                        else 
+                        {
+                            destination = "unknownDestination_" + unknownDestinations.Count;
+                            unknownDestinations.Add(destination);
+                        }
+
+                        string line = (behaviour.Guard.Method.DeclaringType.Namespace.Equals("Stateless")) ?
+                            string.Format(" {0} -> {1} [label=\"{2}\"];", source, destination, behaviour.Trigger) :
+                            string.Format(" {0} -> {1} [label=\"{2} [{3}]\"];", source, destination, behaviour.Trigger, behaviour.Guard.Method.Name);
+
+                        lines.Add(line);
                     }
                 }
             }
 
+            if (unknownDestinations.Any()) 
+            {
+                string label = string.Format(" {{ node [label=\"?\"] {0} }};", string.Join (" ", unknownDestinations));
+                lines.Insert(0, label);
+            }
+
             return "digraph {" + System.Environment.NewLine +
-                     string.Join(System.Environment.NewLine, transitions) + System.Environment.NewLine +
+                     string.Join(System.Environment.NewLine, lines) + System.Environment.NewLine +
                    "}";
         }
     }
