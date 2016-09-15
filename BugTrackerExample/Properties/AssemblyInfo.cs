baf1c53f828557b2366b42621f4875dfe62ef4ed
Artur Karbone <artur.karbone@gmail.com>
2016-09-15 18:45:57 +0300

    PR updated after review
    A short summary:
    - Converted samples to .NET Core projects
    - Targeting core/net451/net40 frameworks for Stateless
    - Targeting core/net451 frameworks for Stateless.Tests (current nunit
    runnest does not support 4.0)
    - Cleaning up some mess: like getting rid of obolete csproj files, etc.

--- BugTrackerExample/Properties/AssemblyInfo.cs
+++ BugTrackerExample/Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BugTrackerExample")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("BugTrackerExample")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2009")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("44a44963-d8ec-44a3-b382-69fa3d5b52d9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("1e7bda62-0bef-49c4-badf-f271755d3990")]
