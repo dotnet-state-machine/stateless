6aecd344b5b6125717b03afd41a6b94750e6669e
Edgars Pivovarenoks <edgarsp@jitt.lv>
2016-12-15 12:18:01 +0200

    Project structure consitentency of transition guards

--- src/Stateless/TransitionGuard.cs
+++ src/Stateless/TransitionGuard.cs
@@ -6,19 +6,6 @@ namespace Stateless
 {
     public partial class StateMachine<TState, TTrigger>
     {
-        internal class GuardCondition
-        {
-            Func<bool> _guard;
-            string _description;
-            internal GuardCondition(Func<bool> guard, string description)
-            {
-                _guard = guard;
-                _description = description;
-            }
-            internal Func<bool> Guard { get { return _guard; } }
-            internal string Description { get { return _description ?? _guard.TryGetMethodName(); } }
-        }
-
         internal class TransitionGuard
         {
             internal IList<GuardCondition> Conditions { get; private set; }
