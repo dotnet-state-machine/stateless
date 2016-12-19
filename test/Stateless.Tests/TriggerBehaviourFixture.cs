1775cc81654749016b65a86709ec650f37ac57ad
Edgars <edgarsp@jitt.lv>
2016-12-19 22:12:29 +0200

    Multiple guard tests, existing tests bug fixes

--- test/Stateless.Tests/TriggerBehaviourFixture.cs
+++ test/Stateless.Tests/TriggerBehaviourFixture.cs
@@ -19,7 +19,7 @@ namespace Stateless.Tests
         }
 
         [Test]
-        public void WhenGuardConditionFalse_IsGuardConditionMetIsFalse()
+        public void WhenGuardConditionFalse_GuardConditionsMetIsFalse()
         {
             var transtioning = new StateMachine<State, Trigger>.TransitioningTriggerBehaviour(
                 Trigger.X, State.C, () => false);
@@ -28,7 +28,7 @@ namespace Stateless.Tests
         }
 
         [Test]
-        public void WhenGuardConditionTrue_IsGuardConditionMetIsTrue()
+        public void WhenGuardConditionTrue_GuardConditionsMetIsTrue()
         {
             var transtioning = new StateMachine<State, Trigger>.TransitioningTriggerBehaviour(
                 Trigger.X, State.C, () => true);
@@ -37,7 +37,7 @@ namespace Stateless.Tests
         }
 
         [Test]
-        public void WhenOneOfMultipleGuardConditionsFalse_IsGuardConditionMetIsFalse()
+        public void WhenOneOfMultipleGuardConditionsFalse_GuardConditionsMetIsFalse()
         {
             var falseGuard = new[] {
                 new Tuple<Func<bool>, string>(() => true, "1"),
@@ -48,6 +48,20 @@ namespace Stateless.Tests
                 Trigger.X, State.C, new StateMachine<State, Trigger>.TransitionGuard(falseGuard));
 
             Assert.IsFalse(transtioning.GuardConditionsMet);
+        }
+
+        [Test]
+        public void WhenAllGuardConditionsTrue_GuardConditionsMetIsTrue()
+        {
+            var trueGuard = new[] {
+                new Tuple<Func<bool>, string>(() => true, "1"),
+                new Tuple<Func<bool>, string>(() => true, "2")
+            };
+
+            var transtioning = new StateMachine<State, Trigger>.TransitioningTriggerBehaviour(
+                Trigger.X, State.C, new StateMachine<State, Trigger>.TransitionGuard(trueGuard));
+
+            Assert.IsTrue(transtioning.GuardConditionsMet);
         }
     }
 }
