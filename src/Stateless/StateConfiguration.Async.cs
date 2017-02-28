0bdbad420dffcd23a1efde50f49ea1551d705faa
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-03-01 08:35:27 +1000

    Fixed newlines in XDOC

--- src/Stateless/StateConfiguration.Async.cs
+++ src/Stateless/StateConfiguration.Async.cs
@@ -17,7 +17,7 @@ namespace Stateless
             /// Add an internal transition to the state machine. An internal action does not cause the Exit and Entry actions to be triggered, and does not change the state of the state machine
             /// </summary>
             /// <param name="trigger"></param>
-            /// <param name="guard">Function that must return true in order for the\r\n            /// trigger to be accepted.</param>
+            /// <param name="guard">Function that must return true in order for the trigger to be accepted.</param>
             /// <param name="entryAction"></param>
             /// <returns></returns>
             public StateConfiguration InternalTransitionAsyncIf(TTrigger trigger, Func<bool> guard, Func<Transition, Task> entryAction)
@@ -50,7 +50,7 @@ namespace Stateless
             /// </summary>
             /// <typeparam name="TArg0"></typeparam>
             /// <param name="trigger">The accepted trigger</param>
-            /// <param name="guard">Function that must return true in order for the\r\n            /// trigger to be accepted.</param>
+            /// <param name="guard">Function that must return true in order for the trigger to be accepted.</param>
             /// <param name="internalAction">The asynchronous action performed by the internal transition</param>
             /// <returns></returns>
             public StateConfiguration InternalTransitionAsyncIf<TArg0>(TTrigger trigger, Func<bool> guard, Func<Transition, Task> internalAction)
@@ -67,7 +67,7 @@ namespace Stateless
             /// </summary>
             /// <typeparam name="TArg0"></typeparam>
             /// <param name="trigger">The accepted trigger</param>
-            /// <param name="guard">Function that must return true in order for the\r\n            /// trigger to be accepted.</param>
+            /// <param name="guard">Function that must return true in order for the trigger to be accepted.</param>
             /// <param name="internalAction">The asynchronous action performed by the internal transition</param>
             /// <returns></returns>
             public StateConfiguration InternalTransitionAsyncIf<TArg0>(TriggerWithParameters<TArg0> trigger, Func<bool> guard, Func<TArg0, Transition, Task> internalAction)
@@ -85,7 +85,7 @@ namespace Stateless
             /// <typeparam name="TArg0"></typeparam>
             /// <typeparam name="TArg1"></typeparam>
             /// <param name="trigger">The accepted trigger</param>
-            /// <param name="guard">Function that must return true in order for the\r\n            /// trigger to be accepted.</param>
+            /// <param name="guard">Function that must return true in order for the trigger to be accepted.</param>
             /// <param name="internalAction">The asynchronous action performed by the internal transition</param>
             /// <returns></returns>
             public StateConfiguration InternalTransitionAsyncIf<TArg0, TArg1>(TriggerWithParameters<TArg0, TArg1> trigger, Func<bool> guard, Func<TArg0, TArg1, Transition, Task> internalAction)
@@ -106,7 +106,7 @@ namespace Stateless
             /// <typeparam name="TArg1"></typeparam>
             /// <typeparam name="TArg2"></typeparam>
             /// <param name="trigger">The accepted trigger</param>
-            /// <param name="guard">Function that must return true in order for the\r\n            /// trigger to be accepted.</param>
+            /// <param name="guard">Function that must return true in order for the trigger to be accepted.</param>
             /// <param name="internalAction">The asynchronous action performed by the internal transition</param>
             /// <returns></returns>
             public StateConfiguration InternalTransitionAsyncIf<TArg0, TArg1, TArg2>(TriggerWithParameters<TArg0, TArg1, TArg2> trigger, Func<bool> guard, Func<TArg0, TArg1, TArg2, Transition, Task> internalAction)
