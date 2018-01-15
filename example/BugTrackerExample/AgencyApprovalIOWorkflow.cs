3bff8be682ea6acef69d3d7339474ae41ced28b2
Xiaoguo Ge <xiaoguo.ge@adslot.com>
2018-01-15 12:31:43 +1100

    IOworkflow Examples

---
+++ example/BugTrackerExample/AgencyApprovalIOWorkflow.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Stateless;
+
+namespace BugTrackerExample
+{
+    public class AgencyApprovalIOWorkflow
+    {
+        private enum State
+        {
+            Draft,
+            ReadyForAgencyApproval,
+            AgencyApproved,
+        }
+
+        private State _currentState;
+        private StateMachine<State, IOWorkflowTriggers> _stateMachine;
+        StateMachine<State, IOWorkflowTriggers>.TriggerWithParameters<string> _assignAction;
+
+        public AgencyApprovalIOWorkflow()
+        {
+            _stateMachine = new StateMachine<State, IOWorkflowTriggers>(() => _currentState, newState => _currentState = newState);
+            _currentState = State.Draft; //hydrate initial state from DB
+
+            _stateMachine.Configure(State.Draft)
+                .OnEntry(OnEnter_Draft)
+                .Permit(IOWorkflowTriggers.MakeReadyForAgencyApproval, State.ReadyForAgencyApproval);
+
+            _stateMachine.Configure(State.ReadyForAgencyApproval)
+                .OnEntry(OnEnter_AgencyApproved)
+                .Permit(IOWorkflowTriggers.AgencyApprove, State.AgencyApproved)
+                .Permit(IOWorkflowTriggers.AgencyReject, State.Draft);
+
+            _stateMachine.Configure(State.AgencyApproved)
+                .OnEntry(OnEnter_AgencyApproved);
+        }
+
+        public void MakeReadyForApproval()
+        {
+            _stateMachine.Fire(IOWorkflowTriggers.MakeReadyForAgencyApproval);
+        }
+
+        public IEnumerable<IOWorkflowTriggers> PermittedTriggers()
+        {
+            return _stateMachine.PermittedTriggers;
+        }
+
+        public void Approve()
+        {
+            _stateMachine.Fire(IOWorkflowTriggers.AgencyApprove);
+        }
+
+        public void Reject()
+        {
+            _stateMachine.Fire(IOWorkflowTriggers.AgencyReject);
+        }
+
+        private void OnEnter_Draft()
+        {
+            Debug.WriteLine($"writing {_currentState} to DB");
+        }
+
+        private void OnEnter_ReadyForAgencyApproval()
+        {
+            Debug.WriteLine($"writing {_currentState} to DB");
+        }
+
+        private void OnEnter_AgencyApproved()
+        {
+            Debug.WriteLine($"writing {_currentState} to DB");
+        }
+    }
+}
--- example/BugTrackerExample/Program.cs
+++ example/BugTrackerExample/Program.cs
@@ -9,19 +9,11 @@ namespace BugTrackerExample
     {
         static void Main(string[] args)
         {
-            var bug = new Bug("Incorrect stock count");
 
-            bug.Assign("Joe");
-            bug.Defer();
-            bug.Assign("Harry");
-            bug.Assign("Fred");
-            bug.Close();
-
-            Console.WriteLine();
-            Console.WriteLine("State machine:");
-            Console.WriteLine(bug.ToDotGraph());
-
-            Console.ReadKey(false);
+            var ioWorkflow = new AgencyApprovalIOWorkflow();
+            ioWorkflow.MakeReadyForApproval();
+            ioWorkflow.Approve();
+            ioWorkflow.Reject();
         }
     }
 }
