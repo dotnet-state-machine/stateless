28b2ab7352e92bbdb0742b84bc32fc65314fb5d8
Ronald Wildenberg <rwwilden@gmail.com>
2018-07-23 10:50:37 +0200

    Opened up TaskResult.FromResult because needed by other parts of library

--- src/Stateless/TaskResult.cs
+++ src/Stateless/TaskResult.cs
@@ -6,7 +6,7 @@ namespace Stateless
     {
         internal static readonly Task Done = FromResult(1);
 
-        static Task<T> FromResult<T>(T value)
+        internal static Task<T> FromResult<T>(T value)
         {
             var tcs = new TaskCompletionSource<T>();
             tcs.SetResult(value);
