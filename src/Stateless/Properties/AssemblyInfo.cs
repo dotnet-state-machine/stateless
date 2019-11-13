6489a93200b9eb5d973e2d32db1fc9d4f4c94fc6
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2019-11-13 20:07:33 +1000

    Restore explanatory comment

--- src/Stateless/Properties/AssemblyInfo.cs
+++ src/Stateless/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
+// This value will remain fixed throughout the 4.x series.
 [assembly: AssemblyVersion("4.0.0.0")]
 
 [assembly: InternalsVisibleTo("Stateless.Tests, PublicKey=" +
