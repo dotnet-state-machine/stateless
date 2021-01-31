c5e2f283bcbfeddda835bc927bcc29fd1a5b3fb8
HenningNT <henning.negaardtorsteinsen@gmail.com>
2021-01-31 10:29:51 +0100

    Trying to fix multiple calls to guard function in async internal tran… (#420)
    * Trying to fix multiple calls to guard function in async internal transition.
    
    * Basically copied over the sync InternalFireOne and made it async. Copied over accompanying sync methods, and made them async.
    Simplified StateRepresentation.Async
    
    * Update pipeline with 2.1 dotnet version.
    
    * use ms build for builds
    
    * Uses microsoft setup path
    
    * name right place!
    
    * running msbuild with restore
    
    * Reverted EnterAsync
    
    * Fixing tests build and testing.
    
    * Use Windows latest to build .net framework
    
    * Update BuildAndTestOnPullRequests.yml
    
    * Update BuildAndTestOnPullRequests.yml
    
    use backslash ???
    
    * Update BuildAndTestOnPullRequests.yml
    
    * Update BuildAndTestOnPullRequests.yml
    
    make version number
    
    * Update BuildAndTestOnPullRequests.yml
    
    set VERSION env
    
    * Update BuildAndTestOnPullRequests.yml
    
    * Update BuildAndTestOnPullRequests.yml
    
    * Update BuildAndTestOnPullRequests.yml
    
    * Update BuildAndTestOnPullRequests.yml
    
    * Update BuildAndTestOnPullRequests.yml
    
    remove version nuymber for now...
    
    Co-authored-by: Henning Torsteinsen <henning.torsteinsen@bouvet.no>

--- src/Stateless/StateRepresentation.Async.cs
+++ src/Stateless/StateRepresentation.Async.cs
@@ -75,6 +75,7 @@ namespace Stateless
                     await action.ExecuteAsync().ConfigureAwait(false);
             }
 
+
             public async Task EnterAsync(Transition transition, params object[] entryArgs)
             {
                 if (transition.IsReentry)
@@ -92,29 +93,21 @@ namespace Stateless
 
             public async Task<Transition> ExitAsync(Transition transition)
             {
-                if (transition.IsReentry)
+                await ExecuteExitActionsAsync(transition).ConfigureAwait(false);
+                if (_superstate != null && !Includes(transition.Destination))
                 {
-                    await ExecuteExitActionsAsync(transition).ConfigureAwait(false);
-                }
-                else if (!Includes(transition.Destination))
-                {
-                    await ExecuteExitActionsAsync(transition).ConfigureAwait(false);
-
-                    if (_superstate != null)
+                    // Check if destination is within the state list
+                    if (IsIncludedIn(transition.Destination))
                     {
-                        // Check if destination is within the state list
-                        if (IsIncludedIn(transition.Destination))
-                        {
-                            // Destination state is within the list, exit first superstate only if it is NOT the the first
-                            if (!_superstate.UnderlyingState.Equals(transition.Destination))
-                            {
-                                return await _superstate.ExitAsync(transition).ConfigureAwait(false);
-                            }
-                        }
-                        else
+                        // Destination state is within the list, exit first superstate only if it is NOT the first
+                        if (!_superstate.UnderlyingState.Equals(transition.Destination))
                         {
                             return await _superstate.ExitAsync(transition).ConfigureAwait(false);
                         }
+                    }
+                    else
+                    {
+                        return await _superstate.ExitAsync(transition).ConfigureAwait(false);
                     }
                 }
                 return transition;
@@ -130,34 +123,6 @@ namespace Stateless
             {
                 foreach (var action in ExitActions)
                     await action.ExecuteAsync(transition).ConfigureAwait(false);
-            }
-
-            async Task ExecuteInternalActionsAsync(Transition transition, object[] args)
-            {
-                InternalTriggerBehaviour internalTransition = null;
-
-                // Look for actions in superstate(s) recursivly until we hit the topmost superstate, or we actually find some trigger handlers.
-                StateRepresentation aStateRep = this;
-                while (aStateRep != null)
-                {
-                    if (aStateRep.TryFindLocalHandler(transition.Trigger, args, out TriggerBehaviourResult result))
-                    {
-                        // Trigger handler(s) found in this state
-                        internalTransition = result.Handler as InternalTriggerBehaviour;
-                        break;
-                    }
-                    // Try to look for trigger handlers in superstate (if it exists)
-                    aStateRep = aStateRep._superstate;
-                }
-
-                // Execute internal transition event handler
-                if (internalTransition == null) throw new ArgumentNullException("The configuration is incorrect, no action assigned to this internal transition.");
-                await (internalTransition.ExecuteAsync(transition, args)).ConfigureAwait(false);
-            }
-
-            internal Task InternalActionAsync(Transition transition, object[] args)
-            {
-                return ExecuteInternalActionsAsync(transition, args);
             }
         }
     }
