a5a35cae3a1c6dbdb90ccf99caeb3bba05e3b602
Lee Oades <lee.oades@starlizard.com>
2023-04-24 17:22:25 +0100

    Remove unused using declarations

--- test/Stateless.Tests/SynchronizationContextFixture.cs
+++ test/Stateless.Tests/SynchronizationContextFixture.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
